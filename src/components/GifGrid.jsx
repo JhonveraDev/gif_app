f7da4ca48a05a31451025af3c945ee7c88dae02a
Jhon Vera <seguridad.veracontainer@gmail.com>
2025-03-28 15:58:40 -0500

    Loading Added

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,27 +1,17 @@
-import { useEffect, useState } from "react"
-import { getGifs } from "../helpers/GetGifs"
 import { GifItem } from "./GifItem"
-
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
 
-  const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages)
-  }
-
-  useEffect(() => {
-    getImages();
-  }, []);
+  const { images, isLoading } = useFetchGifs(category)
 
   return (
     <>
       <h4>{category}</h4>
+      {isLoading ? (<h2>Cargando...</h2>) : null}
       <div className="card-grid">
         {images.map((image) => (
-          <GifItem key={image.id} {... image} />
+          <GifItem key={image.id} {...image} />
         ))}
       </div>
     </>
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,22 +1,22 @@
 export const getGifs = async (category) => {
   const API_KEY = "lVv58xozg2RLx74t7heIkXqwHcpoYFLY";
   const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${encodeURIComponent(category)}&limit=5`;
-    
-    const resp = await fetch(url);
-    if (!resp.ok) {
-      console.error("Error en la petición:", resp.status);
-      return;
-    }
-  
-    const { data } = await resp.json();
-  
-    const gifs = data.map(img => ({
-      id: img.id,
-      title: img.title,
-      url: img.images.downsized_medium.url
-    }))
-  
-    console.log(gifs);
-  
-    return gifs;
-  };
+
+  const resp = await fetch(url);
+  if (!resp.ok) {
+    console.error("Error en la petición:", resp.status);
+    return;
+  }
+
+  const { data } = await resp.json();
+
+  const gifs = data.map(img => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url
+  }))
+
+  console.log(gifs);
+
+  return gifs;
+};
